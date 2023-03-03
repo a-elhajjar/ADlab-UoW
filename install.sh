d5dae70e66845415676982cde44224140a95cdb8
a-elhajjar <31469627+a-elhajjar@users.noreply.github.com>
2023-03-03 08:23:36 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
-#!/bin/sh
-echo 'Please select from the server list:'
-nl server.list
-count="$(wc -l server.list | cut -f 1 -d' ')"
-n=""
-while true; do
-    read -p 'Select option: ' n
-    # If $n is an integer between one and $count...
-    if [ "$n" -eq "$n" ] && [ "$n" -gt 0 ] && [ "$n" -le "$count" ]; then
-        break
-    fi
+#!/bin/bash
+
+SCRIPT_PATH=$(readlink -f $(dirname $0))
+
+PS3='AD Lab setup- Make sure you do all if needed: '
+options=("UpdateRepository" "Ubuntu" "Arch" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "UpdateRepository")
+            echo "You have chosen to update the repositoy"
+            sudo sudo add-apt-repository 'deb http://kali.cs.nctu.edu.tw/kali kali-rolling main non-free contrib'
+            ;;
+        "Option 2")
+            echo "you chose Ubuntu"
+            ${SCRIPT_PATH}/ubuntu.sh
+            ;;
+        "Option 3")
+            echo "you chose Arch $REPLY which is $opt"
+            ${SCRIPT_PATH}/arch.sh
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
 done
-value="$(sed -n "${n}p" server.list)"
-echo "The user selected option number $n: '$value'"
